6991859e51c5d56a25b0e081afce579116ebaef1
cyberw0lf <dubey.abhineet@gmail.com>
2019-06-28 00:42:45 +0530

    updating with new solutions

--- algorithms/implementation/picking_numbers.java
+++ algorithms/implementation/picking_numbers.java
@@ -7,19 +7,18 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 class Result {
-
-    /*
-     * Complete the 'pickingNumbers' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts INTEGER_ARRAY a as parameter.
-     */
-
+// 4 6 5 3 3 1
     public static int pickingNumbers(List<Integer> a) {
-        for(int i=0; i<a.size(); i++){
-          
+        Collection.sort(a);
+        int elementCount = 0;
+        for(int i=0; i<a.size()-1; i++){
+          for(int j=i+1; j<a.size(); j++){
+            if(Math.abs(a.get(i)-a.get(j))>1) break;
+            int tempCount = j-i;
+            if(tempCount>elementCount) elementCount = tempCount;
+          }
         }
-
+        return elementCount+1;
     }
 
 }
